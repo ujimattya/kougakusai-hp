b3db4be038c178ce55073a6fba3ff61e991a109a
森本拓朗 <mori.taku9531@gmail.com>
2020-10-23 18:32:55 +0900

    [add]metaタグ追加

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,9 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
         <link rel="icon" href="/samu.svg" />
-
+        <title>2020年こうがく祭公式HP|茨城大学</title>
+        <meta content="茨城大学の工学部による学園祭、こうがく祭の2020年度版公式ホームページです。今年は11月7日にオンラインで開催されます。こうがく祭についての情報を発信していきます。" name="description"/>
+        <meta charset="utf-8"/>
         </Head>
         <body>
           <div>
