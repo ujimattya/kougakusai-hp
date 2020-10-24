472e0251dac0d898bd1a2468d1621c190a6c9dbe
森本拓朗 <mori.taku9531@gmail.com>
2020-10-24 19:44:55 +0900

    [add]ogp設定

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,12 @@ class MyDocument extends Document {
         <title>2020年こうがく祭公式HP|茨城大学</title>
         <meta content="茨城大学の工学部による学園祭、こうがく祭の2020年度版公式ホームページです。今年は11月7日にオンラインで開催されます。こうがく祭についての情報を発信していきます。" name="description"/>
         <meta charset="utf-8"/>
+        <meta property="og:url" content="https://kougakusai.jp/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content=" 2020年こうがく祭公式HPのトップページ" />
+        <meta property="og:description" content=" 茨城大学の工学部による学園祭、こうがく祭の2020年度版公式ホームページです。今年は11月7日にオンラインで開催されます。" />
+        <meta property="og:site_name" content="2020年こうがく祭公式HP|茨城大学" />
+        <meta property="og:image" content=" https://kougakusai.jp/og.png/" />
         </Head>
         <body>
           <div>
